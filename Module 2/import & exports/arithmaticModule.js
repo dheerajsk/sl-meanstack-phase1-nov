56acb8d3a877d899eb11735a7ff21665b7828c12
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2021-12-04 21:05:37 +0530

    export import

---
+++ Module 2/import & exports/arithmaticModule.js
@@ -0,0 +1,17 @@
+
+const obj = {
+    add: function(a, b){
+        return a+b;
+    },
+    sub: function(a, b){
+        return a-b;
+    },
+    mul: function(a, b){
+        return a*b;
+    },
+    div: function(a, b){
+        return a/b;
+    }
+};
+
+module.exports = obj;
---
+++ Module 2/import & exports/mainModule.js
@@ -0,0 +1,7 @@
+
+const arithModule = require("./arithmaticModule");
+
+console.log("Add: "+arithModule.add(1, 2));
+console.log("Sub: "+arithModule.sub(4, 2));
+console.log("Mul: "+arithModule.mul(3, 2));
+console.log("Div: "+arithModule.div(6, 2));
